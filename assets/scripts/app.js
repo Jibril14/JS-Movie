bf27c5a9beba244b83bf6ddaae6a1a278de363d1
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2019-03-19 00:59:04 +0100

    Add Modal Funtionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,26 @@
-const somevar = "  apple";
+const addMovieModal = document.getElementById("add-modal");
+
+const movieButton = document.querySelector("header").lastElementChild;
+
+const backdropElement = document.body.firstElementChild;
+
+const cancelButtonElement = addMovieModal.querySelector(".btn--passive");
+const addButtonElement = cancelButtonElement.nextElementSibling;
+const allInputElements = document.querySelectorAll();
+
+function addMovie() {
+    addMovieModal.classList.toggle("visible");
+    addbackdrop();
+}
+
+const addbackdrop = () => {
+    backdropElement.classList.toggle("visible");
+};
+
+const backdropClickHandler = () => {
+    addMovie();
+};
+
+movieButton.addEventListener("click", addMovie);
+backdropElement.addEventListener("click", addMovie);
+cancelButtonElement.addEventListener("click", addMovie);
