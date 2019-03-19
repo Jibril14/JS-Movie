b2ee51557929d1140cdf09548719fb3fdf6e21d0
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2019-03-19 02:02:29 +0100

    Added Movie Handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,9 +6,9 @@ const backdropElement = document.body.firstElementChild;
 
 const cancelButtonElement = addMovieModal.querySelector(".btn--passive");
 const addButtonElement = cancelButtonElement.nextElementSibling;
-const allInputElements = document.querySelectorAll();
+const allInputElement = document.querySelectorAll("input");
 
-function addMovie() {
+function toggleMovieModal() {
     addMovieModal.classList.toggle("visible");
     addbackdrop();
 }
@@ -18,9 +18,45 @@ const addbackdrop = () => {
 };
 
 const backdropClickHandler = () => {
-    addMovie();
+    toggleMovieModal();
 };
 
-movieButton.addEventListener("click", addMovie);
-backdropElement.addEventListener("click", addMovie);
-cancelButtonElement.addEventListener("click", addMovie);
+const clearUserInput = () => {
+    for (const inp of allInputElement) {
+        inp.value = "";
+    }
+};
+
+const movie = [];
+
+const addMovieHandler = () => {
+    movieTitle = allInputElement[0].value;
+    movieImage = allInputElement[1].value;
+    movieRating = allInputElement[2].value;
+
+    if (
+        movieTitle.trim() === "" ||
+        movieImage.trim() === "" ||
+        movieRating.trim() === "" ||
+        +movieRating < 1 ||
+        +movieRating > 5
+    ) {
+        alert("Please enter valid values");
+        return;
+    }
+    const newMovie = {
+        title: movieTitle,
+        image: movieImage,
+        rating: movieRating
+    };
+
+    movie.push(newMovie);
+    console.log(newMovie);
+    toggleMovieModal();
+    clearUserInput();
+};
+
+movieButton.addEventListener("click", toggleMovieModal);
+backdropElement.addEventListener("click", toggleMovieModal);
+cancelButtonElement.addEventListener("click", toggleMovieModal);
+addButtonElement.addEventListener("click", addMovieHandler);
